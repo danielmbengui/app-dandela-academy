0b3c5d69d2d1a190a84158533bd4ca3f4d5afc52
danielmbengui <daniel.mbengui@gmail.com>
2025-11-19 21:51:44 +0100

    cleaning classes

--- src/classes/users/ClassUser.js
+++ src/classes/users/ClassUser.js
@@ -16,8 +16,6 @@ import {
 } from "firebase/firestore";
 import { firestore } from "@/contexts/firebase/config";
 import { defaultLanguage } from "@/contexts/i18n/settings";
-import { PAGE_DASHBOARD_HOME, PAGE_DASHBOARD_LESSONS, PAGE_DASHBOARD_PROFILE, PAGE_DASHBOARD_STUDENTS, PAGE_DASHBOARD_TUTORS } from "@/contexts/constants/constants_pages";
-import { IconHome, IconLessons, IconProfile, IconStudents, IconTutors } from "@/assets/icons/IconsComponent";
 import { isValidEmail, parseAndValidatePhone } from "@/contexts/functions";
 
 export class ClassUser {
@@ -309,36 +307,11 @@ export class ClassUser {
     }
     /**************** PAGES ****************/
     pageDashboard() {
-        return (PAGE_DASHBOARD_HOME)
+        return;
     }
     /**************** MENU ****************/
     menuDashboard() {
         return [
-            {
-                name: "dashboard",
-                path: PAGE_DASHBOARD_HOME,
-                icon: <IconHome width={22} height={22} />,
-            },
-            {
-                name: "lessons",
-                path: PAGE_DASHBOARD_LESSONS,
-                icon: <IconLessons width={18} height={18} />,
-            },
-            {
-                name: "tutors",
-                path: PAGE_DASHBOARD_TUTORS,
-                icon: <IconTutors width={18} height={18} />,
-            },
-            {
-                name: "students",
-                path: PAGE_DASHBOARD_STUDENTS,
-                icon: <IconStudents width={18} height={18} />,
-            },
-            {
-                name: "profile",
-                path: PAGE_DASHBOARD_PROFILE,
-                icon: <IconProfile width={22} height={22} />,
-            },
             /*
             {
                 name: "settings",
