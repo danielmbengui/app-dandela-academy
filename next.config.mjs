6266837c08cfbd8183d196ca252f9e1f6651823b
danielmbengui <daniel.mbengui@gmail.com>
2025-11-19 21:06:53 +0100

    first commit without tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'flagcdn.com' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'storage.googleapis.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'dnznrvs05pmza.cloudfront.net' },
+      { protocol: 'https', hostname: 'randomuser.me' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'google.com' },
+      { protocol: 'https', hostname: 'fr.wikipedia.org' },
+      { protocol: 'https', hostname: 'www.google.com' },
+      { protocol: 'https', hostname: 'cdn.futura-sciences.com' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'randomuser.me' },
+      { protocol: 'https', hostname: 'kellerfahnen.ch' },
+      //https://fr.wikipedia.org/wiki/Google#/media/Fichier:Google_Favicon_2025.svg
+    ],
+  },
 };
 
 export default nextConfig;
